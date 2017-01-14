f811944cf4b22d1cec763a82cd0c9df741e6b2b4
Adam Douglass <douglass@malloc.ca>
2017-01-14 12:45:36 -0800

    Prune variables.
    Remove unused variables.
    Remove variables externed but not used in more than one place.

--- src/dwsolv.c
+++ src/dwsolv.c
@@ -44,16 +44,9 @@ int         mingw_base_len;
 
 int  DW_setup_error = FALSE;
 int  sysResult;
-int  S_embed;
 int  start_;
-int  UseDwave = FALSE;
-int  DwaveQubo;
 int  my_pid_;
 char DWcommand[256];
-char filename_b[256], filename_result[256];
-char filename_epqmi_max[256];
-char filename_max_full[256];
-char linebuf[256];
 FILE *f, *fr, *fs;
 int  i, j, k, l; // working integer set
 
@@ -61,6 +54,8 @@ void dw_init()
 {
 	// checks to see that all environment variables are in place, along with
 	// .epqmi file and path needed to embed problems to the solver
+	char filename_max_full[256];
+	char linebuf[256];
 
 	mingw_base_len = strlen("MINGWGINK"); // more Ridiculous MinGW workaround
 	my_pid_ = getpid(); // will use for /tmp filename base
@@ -80,19 +75,19 @@ void dw_init()
 		printf(" dw wspath not set up \n");
 		DW_setup_error = TRUE;
 	} else {
-		
-		if (strncmp(wspath, mingw_base, mingw_base_len) == 0) 
+
+		if (strncmp(wspath, mingw_base, mingw_base_len) == 0)
 			wspath += mingw_base_len; // completed RMWorkaround
 
 		sprintf(filename_max_full, "%s/.max_full", workspace);
-		
+
 		if ((fs = fopen(filename_max_full, "r")) == NULL) {
-			printf(" no file %s\n", filename_max_full); 
+			printf(" no file %s\n", filename_max_full);
 			exit(9);
 		}
-		
+
 		if ( fscanf(fs, "%s", linebuf) == 0 ) {
-			DL; printf("fscanf error"); 
+			DL; printf("fscanf error");
 			exit(9);
 		}
 
@@ -106,17 +101,20 @@ void dw_init()
 	}
 	if ( DW_setup_error == TRUE ) {
 		printf(" dw not set up not complete, and -S 0 set \n");
-		DL; printf(" Exiting\n"); 
+		DL; printf(" Exiting\n");
 		exit(9);
 	}
 
+	char filename_epqmi_max[256];
 	sprintf(filename_epqmi_max, "%s/%s/.epqmi_max", workspace, ws_tmp_path); // find the size of embeded file
 	if ((fs = fopen(filename_epqmi_max, "r")) == NULL) {
-		printf(" no file %s\n", filename_epqmi_max); 
+		printf(" no file %s\n", filename_epqmi_max);
 		exit(9);
 	}
+
+	int  S_embed;
 	if ( fscanf(fs, "%d", &S_embed) == 0 ) {
-		DL; printf("fscanf error"); 
+		DL; printf("fscanf error");
 		exit(9);
 	}
 	SubMatrix_ = S_embed;
@@ -125,7 +123,7 @@ void dw_init()
 	if ( (sysResult = setenv(DW_INTERNAL__WSPATH, ws_tmp_path, 1)) != 0 ) {
 		printf(" result of call %d\n", sysResult);
 		printf(" Error making setenv call %s %s \n", "DW_INTERNAL_WSPATH", ws_tmp_path);
-		DL; printf(" setenv command failed \n"); 
+		DL; printf(" setenv command failed \n");
 		exit(9);
 	}
 	strcpy(tmp_path, "/tmp");
@@ -134,7 +132,7 @@ void dw_init()
 
 	if ( (SubMatrix_ < 10) | (SubMatrix_ > 100) ) {
 		DL; printf(" Retrieved a incorrect embedding size, %d  \n", SubMatrix_);
-		printf(" Exiting\n"); 
+		printf(" Exiting\n");
 		exit(9);
 	}
 
@@ -156,10 +154,13 @@ void dw_init()
 void dw_solver(double **val, int maxNodes, short *Q)
 {
 	// bind to .epqmi
+	char filename_b[256];
+	char filename_result[256];
+
 	sprintf(filename_b, "%s/qbs%d.b", tmp_path, my_pid_); // we will need to write b file to /tmp directory
 
 	if ((f = fopen(filename_b, "w")) == NULL ) {
-		DL; printf("  Failed open %s\n", filename_b); 
+		DL; printf("  Failed open %s\n", filename_b);
 		exit(9);
 	}
 
