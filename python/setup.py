d30d98e4ebefb587e1229ccfa33d5444d659b61d
Alexander Condello <acondello@dwavesys.com>
2017-09-18 09:40:01 -0700

    requires -> install_requires

--- python/setup.py
+++ python/setup.py
@@ -23,6 +23,6 @@ extensions = [Extension("dwave_qbsolv.qbsolv",
 setup(name='dwave_qbsolv',
       version='0.1.2',
       packages=find_packages(),
-      requires=['cython',
-                'dimod'],
+      install_requires=['dimod>=0.1.1',
+                        'cython'],
       ext_modules=cythonize(extensions))
