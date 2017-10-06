136aead205e26812da630c626f891b119fad8700
Alexander Condello <acondello@dwavesys.com>
2017-10-06 10:56:07 -0700

    refactor of ad2_qubo

--- examples/mapColoringUSStates/adj2qubo.py
+++ examples/mapColoringUSStates/adj2qubo.py
@@ -20,84 +20,81 @@ import os
 
 def ad2_qubo(qubo_out, adj_in):
 
-    max_states = 1000
-    qubo_Records = [0] * max_states
-    states_records = [0] * max_states
+    # First let's load the adjacency from the file. We store the adjacency
+    # information in a dictionary where the keys are the states and the
+    # values are sets such that if state0 and state1 are adjacent,
+    # (state0 in state_adj[state1]) == True
+    # We also want to map each state to an integer index, we store this mapping
+    # in state_to_index
+    state_adj = {}
+    state_to_index = {}
+    index_to_state = {}
+    idx = 0
+    with open(adj_in, "r") as adj:
+        for line in adj:
+            if line[0] == 'c' or line[0] == '#':
+                # skip the comment lines
+                continue
 
-    states_adj = [0] * max_states
-    states_couplers = [0] * max_states
-    states = [0] * max_states
-    states_num = [0] * max_states
-    qubo = open(qubo_out, "w")
-    adj = open(adj_in, "r")
+            # in each line, there is a comma separated list of states. The
+            # first state is the node, the remaining are the nodes adjacent
+            # the list is bi-directional (e.g. AK,HI and HI,AK are both lines)
+            states = line.strip('\n').split(',')
+            state_adj[states[0]] = set(states[1:])
 
-    adj_lines = []
-    num_states = 0
-    for line in adj:
-        if line[0] == 'c' or line[0] == '#':
-            continue
-        adj_lines.append(line.strip('\n'))
-        num_states += 1
-    num_nodes = 0
-    num_couplers = 0
-    for i in range(num_states):
-        states_records[i] = adj_lines[i].split(',')
-        states_adj[i] = len(states_records[i]) - 1
-        states[i] = "".join(map(str, states_records[i][:1]))
-        states_records[i] = [it for it in states_records[i] if it != states[i]]
-        num_nodes += 1
+            # each state has an integer label, so we record the mapping in both
+            # directions
+            state_to_index[states[0]] = idx
+            index_to_state[idx] = states[0]
+            idx += 1
 
-    for i in range(num_states):
-        state_str = "".join(map(str, states[i]))
-        states_num[i] = i
-        for k in range(num_states):
-            states_records[k] = [it if it != state_str else i for it in states_records[k]]
-            states_records[k] = [it for it in states_records[k] if it > k]
-
-        states_couplers[i] = len(states_records[i])
-        num_couplers += states_couplers[i]
-
-    num_couplers = num_couplers * 4 + num_states * 6
-    num_nodes = num_nodes * 4
+    # we need some information in advance for the qubo, specifically the number of nodes and
+    # the number of couplers
+    num_nodes = 4 * len(state_adj)
+    num_couplers = sum(len(states) for states in state_adj.values()) * 2 + 6 * len(state_adj)
 
     # now time to write the qubo
     # nodes first
-    qubo.write("c" + "\n")
-    qubo.write("c  this qubo was created by adj2qubo.py for 4 color uninary encoding" + "\n")
-    qubo.write("c" + "\n")
-    qubo.write("p  qubo  0  {} {} {}\n".format(num_nodes, num_nodes, num_couplers))
+    with open(qubo_out, "w") as qubo:
+        qubo.write("c" + "\n")
+        qubo.write("c  this qubo was created by adj2qubo.py for 4 color uninary encoding" + "\n")
+        qubo.write("c" + "\n")
+        qubo.write("p  qubo  0  {} {} {}\n".format(num_nodes, num_nodes, num_couplers))
 
-    for st in range(num_states):
-        qubo.write("c " + states[st] + "\n")
-        qubo.write("  " + str(st * 4) + " " + str(st * 4) + " -1 " + "\n")
-        qubo.write("  " + str(st * 4 + 1) + " " + str(st * 4 + 1) + " -1 " + "\n")
-        qubo.write("  " + str(st * 4 + 2) + " " + str(st * 4 + 2) + " -1 " + "\n")
-        qubo.write("  " + str(st * 4 + 3) + " " + str(st * 4 + 3) + " -1 " + "\n")
+        for st in range(len(state_adj)):
+            state = index_to_state[st]
+            qubo.write("c " + state + "\n")
+            qubo.write("  " + str(st * 4) + " " + str(st * 4) + " -1 " + "\n")
+            qubo.write("  " + str(st * 4 + 1) + " " + str(st * 4 + 1) + " -1 " + "\n")
+            qubo.write("  " + str(st * 4 + 2) + " " + str(st * 4 + 2) + " -1 " + "\n")
+            qubo.write("  " + str(st * 4 + 3) + " " + str(st * 4 + 3) + " -1 " + "\n")
 
-    qubo.write("c" + "\n")
-    qubo.write("c  Couplers " + "\n")
-    qubo.write("c" + "\n")
+        qubo.write("c" + "\n")
+        qubo.write("c  Couplers " + "\n")
+        qubo.write("c" + "\n")
 
-    for st in range(num_states):
-        qubo.write("c " + states[st] + "   " + str(states_adj[st]) + " neighbors  " + str(states_couplers[st] * 4) + " external couplers\n")
-        qubo.write("  " + str(st * 4) + " " + str(st * 4 + 1) + " 2 " + "\n")
-        qubo.write("  " + str(st * 4) + " " + str(st * 4 + 2) + " 2 " + "\n")
-        qubo.write("  " + str(st * 4) + " " + str(st * 4 + 3) + " 2 " + "\n")
-        qubo.write("  " + str(st * 4 + 1) + " " + str(st * 4 + 2) + " 2 " + "\n")
-        qubo.write("  " + str(st * 4 + 1) + " " + str(st * 4 + 3) + " 2 " + "\n")
-        qubo.write("  " + str(st * 4 + 2) + " " + str(st * 4 + 3) + " 2 " + "\n")
+        for st in range(len(state_adj)):
+            state = index_to_state[st]
+            neighbors = state_adj[state]
+            qubo.write("c " + state + "   " + str(len(neighbors)) + " neighbors  " + str(len(neighbors) * 4) + " external couplers\n")
+            qubo.write("  " + str(st * 4) + " " + str(st * 4 + 1) + " 2 " + "\n")
+            qubo.write("  " + str(st * 4) + " " + str(st * 4 + 2) + " 2 " + "\n")
+            qubo.write("  " + str(st * 4) + " " + str(st * 4 + 3) + " 2 " + "\n")
+            qubo.write("  " + str(st * 4 + 1) + " " + str(st * 4 + 2) + " 2 " + "\n")
+            qubo.write("  " + str(st * 4 + 1) + " " + str(st * 4 + 3) + " 2 " + "\n")
+            qubo.write("  " + str(st * 4 + 2) + " " + str(st * 4 + 3) + " 2 " + "\n")
 
-        for ext_coup in range(states_couplers[st]):
-            coupl_st = states_records[st][ext_coup]
-            qubo.write("c " + states[st] + " linked to   " + states[coupl_st] + "\n")
+            for ext_coup in sorted(state_to_index[state] for state in neighbors):
+                if ext_coup < st:
+                    continue
+                coupl_st = index_to_state[ext_coup]
+                qubo.write("c " + state + " linked to   " + coupl_st + "\n")
 
-            qubo.write("  " + str(st * 4 + 0) + " " + str(coupl_st * 4 + 0) + " 1 " + "\n")
-            qubo.write("  " + str(st * 4 + 1) + " " + str(coupl_st * 4 + 1) + " 1 " + "\n")
-            qubo.write("  " + str(st * 4 + 2) + " " + str(coupl_st * 4 + 2) + " 1 " + "\n")
-            qubo.write("  " + str(st * 4 + 3) + " " + str(coupl_st * 4 + 3) + " 1 " + "\n")
+                qubo.write("  " + str(st * 4 + 0) + " " + str(ext_coup * 4 + 0) + " 1 " + "\n")
+                qubo.write("  " + str(st * 4 + 1) + " " + str(ext_coup * 4 + 1) + " 1 " + "\n")
+                qubo.write("  " + str(st * 4 + 2) + " " + str(ext_coup * 4 + 2) + " 1 " + "\n")
+                qubo.write("  " + str(st * 4 + 3) + " " + str(ext_coup * 4 + 3) + " 1 " + "\n")
 
-    adj.close()
-    qubo.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Read adj files and create a qubo 4 color map file')
