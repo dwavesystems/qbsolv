b23ed52ca5880b778f499013ebbbd8e7c5ca9a53
Bradley Ellert <bellert@dwavesys.com>
2018-02-14 10:10:14 -0800

    Skip uploading existing files to pypi

--- ci/travis/upload-wheels.sh
+++ ci/travis/upload-wheels.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 pip install twine
-twine upload --username $TWINE_USERNAME --password $TWINE_PASSWORD wheelhouse/*
+twine upload --username $TWINE_USERNAME --password $TWINE_PASSWORD --skip-existing wheelhouse/*
