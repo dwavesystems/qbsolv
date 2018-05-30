24c92bfba509cf6288df40133c8e52fb93aff0d6
Bradley Ellert <bellert@dwavesys.com>
2018-05-30 16:40:06 -0700

    Update old example
    `response` no longer has energies
    Use data_vectors

--- examples/tryDwaveQbsolv.py
+++ examples/tryDwaveQbsolv.py
@@ -2,4 +2,4 @@ from dwave_qbsolv import QBSolv
 Q = {(0, 0): 1, (1, 1): 1, (0, 1): 1}
 response = QBSolv().sample_qubo(Q)
 print("samples=" + str(list(response.samples())))
-print("energies=" + str(list(response.energies())))
+print("energies=" + str(list(response.data_vectors['energy'])))
