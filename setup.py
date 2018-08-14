ea6a43a0231178885eb9e4e09f476c5fcd4db1dd
Alexander Condello <acondello@dwavesys.com>
2018-08-14 10:33:11 -0700

    Update version 0.2.8 -> 0.2.9

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ python_requires = '>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*'
 
 setup(
     name='dwave_qbsolv',
-    version='0.2.8',
+    version='0.2.9',
     packages=packages,
     package_dir={'dwave_qbsolv': 'python/dwave_qbsolv'},
     install_requires=['dimod>=0.6.10,<0.8.0'],
