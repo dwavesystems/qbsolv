0490be84d991f8a37a434e35a6e36dfde7b4deaf
Alexander Condello <acondello@dwavesys.com>
2018-08-14 10:30:51 -0700

    dimod>=0.6.10,<0.8.0

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     version='0.2.8',
     packages=packages,
     package_dir={'dwave_qbsolv': 'python/dwave_qbsolv'},
-    install_requires=['dimod>=0.6.10,<0.7.0'],
+    install_requires=['dimod>=0.6.10,<0.8.0'],
     ext_modules=extensions,
     cmdclass={'build_ext': build_ext_compiler_check},
     long_description=open('readme.md').read(),
