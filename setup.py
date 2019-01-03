2d0f2d8169519312b4473c3c0b450734fefff153
Alexander Condello <acondello@dwavesys.com>
2019-01-03 08:53:27 -0800

    Allow use of dimod 0.8.x

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ else:
 
 extra_compile_args = {
     'msvc': [],
-    'unix': ['-std=c++11', '-Ofast', '-Wall', '-Wextra', '-flto'],
+    'unix': ['-std=c++11', '-Ofast', '-Wall', '-Wextra'],
     # 'unix': ['-std=c++1y','-w','-O0', '-g', '-fipa-pure-const'],
 }
 
@@ -78,7 +78,7 @@ setup(
     version='0.2.9',
     packages=packages,
     package_dir={'dwave_qbsolv': 'python/dwave_qbsolv'},
-    install_requires=['dimod>=0.6.10,<0.8.0'],
+    install_requires=['dimod>=0.6.10,<0.9.0'],
     ext_modules=extensions,
     cmdclass={'build_ext': build_ext_compiler_check},
     long_description=open('README.rst').read(),
