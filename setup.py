7d52048594bcd7757fc7b63176a13e544fc77ba3
arcondello <arcondello@gmail.com>
2020-02-24 09:40:16 -0800

    Update version 0.2.10 -> 0.3.0

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ python_requires = '>=3.5'
 
 setup(
     name='dwave-qbsolv',
-    version='0.2.10',
+    version='0.3.0',
     packages=packages,
     package_dir={'dwave_qbsolv': 'python/dwave_qbsolv'},
     install_requires=['dimod>=0.8.1,<0.10.0'],
