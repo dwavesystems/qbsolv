a406a795663e82645faa82aca1a58a3cf4513e64
Radomir Stevanovic <radomir@dwavesys.com>
2020-09-22 03:55:41 -0700

    Release version 0.3.1
    Fixes
    ---
    
    - Test with dimod 0.9.x by default
    - Stopped using hierarchical requirements include
    - Simplified and updated Windows tests to work with latest AppVeyor
      setup
    - Skip deploying linux/py39 package, as not all dependencies are
      available for py39.

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ python_requires = '>=3.5'
 
 setup(
     name='dwave-qbsolv',
-    version='0.3.0',
+    version='0.3.1',
     packages=packages,
     package_dir={'dwave_qbsolv': 'python/dwave_qbsolv'},
     install_requires=['dimod>=0.8.1,<0.10.0'],
