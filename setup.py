0af286f64aae55bcb03b33924d062c6f7f067334
Alexander Condello <arcondello@gmail.com>
2022-04-14 15:50:07 -0700

    Support dimod<0.13

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     version='0.3.4',
     packages=packages,
     package_dir={'dwave_qbsolv': 'python/dwave_qbsolv'},
-    install_requires=['dimod>=0.8.1,<0.11.0'],
+    install_requires=['dimod>=0.8.1,<0.13.0'],
     ext_modules=extensions,
     cmdclass={'build_ext': build_ext_compiler_check},
     long_description=open('README.rst').read(),
